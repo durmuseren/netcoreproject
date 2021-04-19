06ef303ec9bd2e8baa482b5eb5ff09d40ad98d8f
durmuseren <erendurmuseren06@gmail.com>
2021-04-19 05:15:03 +0300

    net core &efcore
    Net Core ve Entity Framework Code First kullanılarak geliştirilen proje

---
+++ NetCoreProje2/NetCoreProje2/Models/Context.cs
@@ -0,0 +1,20 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreProje2.Models
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-92H2T3D;database=Birim3DB;Integrated security=true;");
+        }
+        public DbSet<Birim> Birims { get; set; }
+        public DbSet<Personel> Personels { get; set; }
+        public DbSet <Admin> Admins { get; set; }
+    }
+}
